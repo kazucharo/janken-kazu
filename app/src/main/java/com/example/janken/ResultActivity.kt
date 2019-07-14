df846467635e316d0d91adab298d6428e11387ee
kazucharo <kazucharo@gmail.com>
2019-07-14 18:49:11 +0900

    後出しじゃんけんアプリについて。 １．じゃんけんでは、少ししてからコンピュータの手を出す。 ２．その際、コンピュータは必ず勝利する。 ３．上記でのやりとりに、メッセージ等の演出を添える。

--- app/src/main/java/com/example/janken/ResultActivity.kt
+++ app/src/main/java/com/example/janken/ResultActivity.kt
@@ -2,6 +2,7 @@ package com.example.janken
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_result.*
 
@@ -16,6 +17,7 @@ class ResultActivity : AppCompatActivity() {
         setContentView(R.layout.activity_result)
         val id = intent.getIntExtra("MY_HAND" , 0)
 
+        //myHandについて
         val myHand : Int
         myHand = when(id) {
             R.id.gu -> {
@@ -33,27 +35,46 @@ class ResultActivity : AppCompatActivity() {
             else -> gu
         }
 
-        //コンピュータの手を決める
-        val comHand = getHand()
-        when(comHand){
-            gu -> comHandImage.setImageResource(R.drawable.com_gu)
-            choki -> comHandImage.setImageResource(R.drawable.com_choki)
-            pa -> comHandImage.setImageResource(R.drawable.com_pa)
-        }
+        //コンピュータの手を決める→getHandにて決まる（初期の手を負けて表示しておく）
+        val comHand = getHand(myHand)
+            when(myHand){
+                gu -> comHandImage.setImageResource(R.drawable.com_choki)
+                choki -> comHandImage.setImageResource(R.drawable.com_pa)
+                pa -> comHandImage.setImageResource(R.drawable.com_gu)
+            }
 
         //勝敗を判定する
         val gameResult = (comHand - myHand + 3) % 3
-        when(gameResult){
-            0 -> resultLable.setText(R.string.result_draw)    //引き分け
-            1 -> resultLable.setText(R.string.result_win)    //勝った場合
-            2 -> resultLable.setText(R.string.result_lose)    //負けた場合
-        }
+
+        //ディレイ処理のHandlerをインポート（後出しとして勝つ手に変更表示させる）
+        Handler().postDelayed(Runnable {
+            when(comHand){
+                gu -> comHandImage.setImageResource(R.drawable.com_gu)
+                choki -> comHandImage.setImageResource(R.drawable.com_choki)
+                pa -> comHandImage.setImageResource(R.drawable.com_pa)
+            }
+            atodasiPon.setText(R.string.pon_text)
+        }, 800)
+
+        //勝敗メッセージはさらに遅めに表示する
+        Handler().postDelayed(Runnable {
+            when(gameResult){
+                0 -> resultLabel.setText(R.string.result_draw)    //引き分け
+                1 -> resultLabel.setText(R.string.result_win)    //勝った場合
+                2 -> resultLabel.setText(R.string.result_lose)    //負けた場合
+            }
+        }, 1600)
 
         backButton.setOnClickListener { finish()}
 
         saveData(myHand , comHand , gameResult)
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val gameCount = pref.getInt("GAME_COUNT", 0)
+        loseCount.setText("${gameCount}回目")
     }
 
+    //ゲームカウントについて、小さなデータの保存
     private fun saveData (myHand : Int , comHand : Int , gameResult : Int){
         val pref = PreferenceManager.getDefaultSharedPreferences(this)
         val gameCount = pref.getInt("GAME_COUNT", 0)
@@ -75,8 +96,9 @@ class ResultActivity : AppCompatActivity() {
               .apply()
     }
 
-    private fun getHand() : Int{
-        var hand = (Math.random() * 3).toInt()
+    //getHandメソッド（必ず勝利する手に変更する）
+    private fun getHand(myHand : Int) : Int{
+        val hand = (myHand + 2 ) % 3
         val pref = PreferenceManager.getDefaultSharedPreferences(this)
         val gameCount = pref.getInt("GAME_COUNT" , 0)
         val winningStreakCount = pref.getInt("WINNING_STREAK_COUNT" , 0)
@@ -85,21 +107,6 @@ class ResultActivity : AppCompatActivity() {
         val beforeLastComHand = pref.getInt("BEFORE_LAST_COM_HAND" , 0)
         val gameResult = pref.getInt("GAME_RESULT" , -1)
 
-        if (gameCount == 1){
-            if (gameResult == 2){
-                while (lastComHand == hand){
-                    hand = (Math.random() * 3).toInt()
-                }
-            } else if (gameResult == 1){
-                hand = (lastMyHand -1 +3) %3
-            }
-        } else if (winningStreakCount > 0){
-            if (beforeLastComHand == lastComHand){
-                while (lastComHand == hand){
-                    hand = (Math.random() * 3).toInt()
-                }
-            }
-        }
         return hand
     }
 }
